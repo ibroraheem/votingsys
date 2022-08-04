b63bcddf7ed9185bf7a99fca296ce43db73abeda
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-08-04 21:06:59 +0100

    reset password added

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,7 @@ const register = async (req, res) => {
     const { surname, firstname, matric, department, password } = req.body
     const isFirstAccount = (await User.countDocuments({})) === 0;
     const role = isFirstAccount ? 'admin' : 'voter';
-    const email = matric.replace('/', '-') + '@students.unilorin.edu.ng'
+    var email = matric.replace('/', '-') + '@students.unilorin.edu.ng'
 
     const user = new User({
         surname,
@@ -92,4 +92,49 @@ const login = async (req, res) => {
     }
 }
 
-module.exports = { register, login, verifyVoter }
+const forgotPassword = async (req, res) => {
+    const { matric } = req.body
+    const user = await User.findOne({ matric })
+    if (!user) {
+        return res.status(400).send({ message: 'Invalid Matric Number' })
+    }
+    const token = jwt.sign({ matric: user.matric, department: user.department, level: user.level, role: user.role, verified: user.verified}, process.env.JWT_SECRET, { expiresIn: '1h' })
+    const transport = nodemailer.createTransport({
+        host: 'smtp.zoho.eu',
+        port: 465,
+        auth: {
+            user: process.env.MAIL,
+            pass: process.env.PASS
+        }
+
+    })
+    const mailOptions = {
+        from: 'NUESA',
+        to: email,
+        subject: 'Reset Password',
+        html: `<h1>Reset Password</h1>
+         <p>Please click the link to reset your password: <a href="http://localhost/reset/${token}">Reset</a></p>
+            <p>If you did not request this, please ignore this email</p>
+            <p>Thank you</p>`
+    }
+    transport.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('Email sent: ' + info.response)
+        }
+    })
+}
+const resetPassword = async (req, res) => {
+    const { password} = req.body
+    const user = await User.findOne({ confirmationCode: req.params.token })
+    if (!user) {
+        return res.status(400).send({ message: 'Invalid Token' })
+    }
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(password, salt)
+    user.save()
+    res.status(200).send({ message: 'Password reset successfully' })
+}
+
+module.exports = { register, login, verifyVoter, resetPassword, forgotPassword }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {login, register, verifyVoter} = require('../controllers/authController')
+const {login, register, verifyVoter, forgotPassword, resetPassword} = require('../controllers/authController')
 
 router.post('/register', register)
 router.post('/login', login)
 router.get('/confirm/:confirmationCode', verifyVoter)
+router.post('/forgot-password', forgotPassword)
+router.post('/reset-password', resetPassword)
 
 module.exports= router
